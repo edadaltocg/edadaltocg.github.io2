0051a3a62328f40c83e7e2bb80827ab84c2b6f81
Eduardo Dadalto <edadaltocg@gmail.com>
2023-04-05 13:45:11 +0200

    contact route

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
 	import { publicVariables } from '$lib/assets/public';
-	import Social from '$lib/components/Social.svelte';
-	export let data: import('./$types').PageData;
 </script>
 
 <article>
-	<h1>{data.about.metadata.title}</h1>
-	<svelte:component this={data.about.default} />
-
-	<h2 class="text-accent-content">Contact</h2>
+	<h1>Contact</h1>
 	<p>
 		You can contact me via email at
 		<a href="mailto:{publicVariables.email}">{publicVariables.email}</a>. Happy to discuss new
