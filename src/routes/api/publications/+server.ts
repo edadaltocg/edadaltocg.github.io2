c07230989d442ae26ad5a3c301eab98408a3e914
Eduardo Dadalto <edadaltocg@gmail.com>
2023-09-07 16:36:03 +0200

    setup publications endpoint

---
+++ src/routes/api/publications/+server.ts
@@ -0,0 +1,68 @@
+import { json } from '@sveltejs/kit';
+
+export const GET = async ({ url }) => {
+    console.log(url)
+    const pathname = url.pathname
+    const jsonFilePaths = import.meta.glob('/src/routes/api/publications/**/*.json')
+    const bibFiles = import.meta.glob('/src/routes/api/publications/**/*.bib')
+
+    // remove template folder with pattern matching
+    delete jsonFilePaths['/src/routes/api/publications/template/metadata.json']
+    delete bibFiles['/src/routes/api/publications/template/citation.md']
+
+    const jsonDataDict = await Promise.all(
+        Object.entries(jsonFilePaths).map(async ([path, resolver]) => {
+            // check if path contrains substring
+            if (!path.includes(pathname)) {
+                return ["", {}]
+            }
+            const { default: jsonData } = await resolver();
+            const jsonPath = path.slice(11, -14);
+            return [jsonPath, jsonData];
+        })
+    );
+
+    const jsonData = {}
+    jsonDataDict.forEach(([path, data]) => {
+        if (path == "") {
+            return
+        }
+        jsonData[path] = data
+    })
+
+    const bibDataDict = await Promise.all(
+        Object.entries(bibFiles).map(async ([path, resolver]) => {
+            if (!path.includes(pathname)) {
+                return ["", {}]
+            }
+            const { default: mdData } = await import(path + '?raw')
+            const mdPath = path.slice(11, -13);
+            return [mdPath, mdData];
+        })
+    );
+
+    const bibData = {}
+    bibDataDict.forEach(([path, data]) => {
+        if (path == "") {
+            return
+        }
+        bibData[path] = data
+    })
+
+    Object.entries(jsonData).forEach(([path, data]) => {
+        jsonData[path] = {
+            ...data,
+            "bib": bibData[path],
+        }
+    })
+    // transform dict to list
+    const jsonDataList = Object.entries(jsonData).map(([path, data]) => {
+        return {
+            ...data,
+            "path": path,
+        }
+    })
+
+
+    return json(jsonDataList);
+};
